fc07a9e88bffb246e28f7c5b339884ffe6647c4d
hm13 <thirteeeeeeeeeeeeen@gmail.com>
2020-04-14 22:47:21 +0800

    support dot cursor

--- js/main.js
+++ js/main.js
@@ -26,10 +26,13 @@ function inRange(x1, y1, r, x2, y2) {
 
 function drawCircle(ctx) {
     ctx.beginPath();
-    x = rand(0 + 20, PLAY_AREA_WIDTH - 20);
-    y = rand(0 + 20, PLAY_AREA_HEIGHT - 20);
     ctx.arc(x, y, r, 0, 2 * Math.PI);
     ctx.stroke();
+}
+
+function updateCirclePosition(ctx) {
+    x = rand(0 + 20, PLAY_AREA_WIDTH - 20);
+    y = rand(0 + 20, PLAY_AREA_HEIGHT - 20);
 }
 
 var Main = new function() {
@@ -38,15 +41,26 @@ var Main = new function() {
     canvas.height = PLAY_AREA_HEIGHT;
     canvas.style.border = "thin solid #000000";
     ctx = canvas.getContext("2d");
-
+    updateCirclePosition();
     drawCircle(ctx);
 
     canvas.addEventListener('click', function(event) {
         pos = getMousePos(canvas, event);
         if ( inRange(x, y, r, pos.x, pos.y) ) {
             ctx.clearRect(0, 0, canvas.width, canvas.height);
+            updateCirclePosition();
             drawCircle(ctx);
         }
     }, false);
+
+    cursor = new Image();
+    cursor.src = "image/dot.svg";
+
+    canvas.addEventListener('mousemove', function(event) {
+        pos = getMousePos(canvas, event);
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        ctx.drawImage(cursor, pos.x - 10, pos.y - 10);
+        drawCircle(ctx);
+    }, false)
 }
 
