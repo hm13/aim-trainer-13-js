cd1b966c82ed99d61e3fbe59d12bd65db0396379
hm13 <thirteeeeeeeeeeeeen@gmail.com>
2020-04-18 19:10:02 +0800

    refactor

--- js/main.js
+++ js/main.js
@@ -1,87 +1,88 @@
-x = 0
-y = 0
-r = 8
-pos = { x: 0, y: 0 }
-PLAY_AREA_WIDTH = window.innerWidth;
-PLAY_AREA_HEIGHT = window.innerHeight;
+var target = {
+    x: window.innerWidth / 2,
+    y: window.innerHeight / 2,
+    r: 8,
+    color: "#FF0000",
+    alpha: 0.4
+};
+
+var reticle = {
+    x: 0,
+    y: 0,
+    r: 5,
+    color: "#00FF00",
+    alpha: 1
+};
 
 function rand(x1 = 0, x2 = 100) {
     return Math.floor(Math.random() * (x2 - x1 + 1)) + x1
 }
 
-function getMousePos(canvas, event) {
-    var rect = canvas.getBoundingClientRect();
-    return {
-        x: event.clientX - rect.left,
-        y: event.clientY - rect.top
-    };
-}
-
-function dist(x1, y1, x2, y2) {
-    return Math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2);
-}
-
 function inRange(x1, y1, r, x2, y2) {
-    return dist(x1, y1, x2, y2) < r;
+    function dist(x1, y1, x2, y2) {
+        return Math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2);
+    }
+    return r >= dist(x1, y1, x2, y2);
 }
 
-function drawTarget(ctx) {
-    ctx.globalAlpha = 0.4;
-
+function drawCircle(ctx, circle) {
+    ctx.globalAlpha = circle.alpha;
     ctx.beginPath();
-    ctx.arc(x, y, r, 0, 2 * Math.PI);
+    ctx.arc(circle.x, circle.y, circle.r, 0, 2 * Math.PI);
     ctx.closePath();
-
-    ctx.fillStyle = "red";
+    ctx.fillStyle = circle.color;
     ctx.fill();
-
     ctx.globalAlpha = 1;
 }
 
-
-function updateTargetPosition(ctx) {
-    x = rand(0 + 20, PLAY_AREA_WIDTH - 20);
-    y = rand(0 + 20, PLAY_AREA_HEIGHT - 20);
+function drawTarget(ctx) {
+    drawCircle(ctx, target);
 }
 
-function drawReticle(ctx, cursor) {
-    ctx.drawImage(cursor, pos.x - 5, pos.y - 5);
+function drawReticle(ctx) {
+    drawCircle(ctx, reticle);
+}
+
+function updateReticlePosition(canvas, event) {
+    var rect = canvas.getBoundingClientRect();
+    var pos = {
+        x: event.clientX - rect.left,
+        y: event.clientY - rect.top
+    };
+    reticle.x = pos.x;
+    reticle.y = pos.y;
+}
+
+function updateTargetPosition(ctx) {
+    target.x = rand(0 + 20, window.innerWidth - 20);
+    target.y = rand(0 + 20, window.innerHeight - 20);
 }
 
 var Main = new function () {
-    canvas = document.getElementById('world');
-    canvas.width = PLAY_AREA_WIDTH;
-    canvas.height = PLAY_AREA_HEIGHT;
+    var canvas = document.getElementById('world');
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
     canvas.style.border = "0px none #000000";
-    ctx = canvas.getContext("2d");
-
-    reticle = new Image();
-    reticle.src = "image/dot.svg";
+    var ctx = canvas.getContext("2d");
 
     canvas.addEventListener('click', function (event) {
-        if (inRange(x, y, r, pos.x, pos.y)) {
-            ctx.clearRect(0, 0, canvas.width, canvas.height);
+        if (inRange(target.x, target.y, target.r, reticle.x, reticle.y)) {
             updateTargetPosition();
-            drawTarget(ctx);
         }
     }, false);
 
     canvas.addEventListener('mousemove', function (event) {
-        pos = getMousePos(canvas, event);
+        updateReticlePosition(canvas, event);
     }, false);
-
-    updateTargetPosition();
-    drawTarget(ctx);
 
     function runloop(timestamp) {
         ctx.clearRect(0, 0, canvas.width, canvas.height);
 
         drawTarget(ctx);
-        drawReticle(ctx, reticle);
+        drawReticle(ctx);
 
         window.requestAnimationFrame(runloop);
     }
 
     window.requestAnimationFrame(runloop);
 }
-
