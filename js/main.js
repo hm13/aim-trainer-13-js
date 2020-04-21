44e3f9684dfd735d4953f8dbfea2eb296f7454ea
hm13 <thirteeeeeeeeeeeeen@gmail.com>
2020-04-21 22:33:18 +0800

    support multiple targets

--- js/main.js
+++ js/main.js
@@ -1,10 +1,20 @@
-var target = {
-    x: window.innerWidth / 2,
-    y: window.innerHeight / 2,
-    r: 8,
-    color: "#FF0000",
-    alpha: 0.4
+var canvas = document.getElementById("world");
+var ctx = canvas.getContext("2d");
+
+var targets = [];
+var targetNumber = 3;
+let generateTarget = function () {
+    return {
+        x: window.innerWidth / 2,
+        y: window.innerHeight / 2,
+        r: 8,
+        color: "#FF0000",
+        alpha: 0.4
+    };
 };
+for (var i = 0; i < targetNumber; i++) {
+    targets.push(generateTarget());
+}
 
 var reticle = {
     x: 0,
@@ -25,7 +35,7 @@ function inRange(x1, y1, r, x2, y2) {
     return r >= dist(x1, y1, x2, y2);
 }
 
-function drawCircle(ctx, circle) {
+function drawCircle(circle) {
     ctx.globalAlpha = circle.alpha;
     ctx.beginPath();
     ctx.arc(circle.x, circle.y, circle.r, 0, 2 * Math.PI);
@@ -35,12 +45,14 @@ function drawCircle(ctx, circle) {
     ctx.globalAlpha = 1;
 }
 
-function drawTarget(ctx) {
-    drawCircle(ctx, target);
+function drawTarget() {
+    for (const target of targets) {
+        drawCircle(target);
+    }
 }
 
-function drawReticle(ctx) {
-    drawCircle(ctx, reticle);
+function drawReticle() {
+    drawCircle(reticle);
 }
 
 function updateReticlePosition(canvas, event) {
@@ -53,33 +65,46 @@ function updateReticlePosition(canvas, event) {
     reticle.y = pos.y;
 }
 
-function updateTargetPosition(ctx) {
+function updateTargetPosition(target) {
     target.x = rand(0 + 20, window.innerWidth - 20);
     target.y = rand(0 + 20, window.innerHeight - 20);
 }
 
-var Main = new function () {
-    var canvas = document.getElementById('world');
+function checkTargetClick() {
+    for (const target of targets) {
+        if (inRange(target.x, target.y, target.r, reticle.x, reticle.y)) {
+            updateTargetPosition(target);
+        }
+    }
+}
+
+function initializeCanvas() {
     canvas.width = window.innerWidth;
     canvas.height = window.innerHeight;
     canvas.style.border = "0px none #000000";
-    var ctx = canvas.getContext("2d");
 
     canvas.addEventListener('click', function (event) {
-        if (inRange(target.x, target.y, target.r, reticle.x, reticle.y)) {
-            updateTargetPosition();
-        }
+        checkTargetClick();
+    }, false);
+
+    canvas.addEventListener('contextmenu', function (event) {
+        event.preventDefault();
+        checkTargetClick();
     }, false);
 
     canvas.addEventListener('mousemove', function (event) {
         updateReticlePosition(canvas, event);
     }, false);
+}
+
+var Main = new function () {
+    initializeCanvas();
 
     function runloop(timestamp) {
         ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-        drawTarget(ctx);
-        drawReticle(ctx);
+        drawTarget();
+        drawReticle();
 
         window.requestAnimationFrame(runloop);
     }
